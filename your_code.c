afb0fabc8c81de9e6a2b46902ad450a033bdbeb4
Michael Dorst <m@mdorst.net>
2020-03-30 12:56:38 -0700

    Add clang-format, format all .c/.h files

--- your_code.c
+++ your_code.c
@@ -1,59 +1,49 @@
 #include "your_code.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 // Write the implementations of the functions that do the real work here
 
-ASTNode* CreateNumNode(int num)
-{
-        ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode));
-        node->type = ASTNODE_NUM; 
-        node->num = num;
-        return node;
+ASTNode *CreateNumNode(int num) {
+  ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
+  node->type = ASTNODE_NUM;
+  node->num = num;
+  return node;
 }
 
-
-ASTNode* CreateIdentNode(char* name)
-{
-    ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
-    node->type = ASTNODE_NUM;
-    node->name = name;
-    return node;
+ASTNode *CreateIdentNode(char *name) {
+  ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
+  node->type = ASTNODE_NUM;
+  node->name = name;
+  return node;
 }
 
 // Take a statement node and a statement list node and connect them together
-// to form a bigger statement list node (add the statement to the statement list).
-// Return a pointer to the bigger list that resulted from this linking
-ASTNode* CreateStatementListNode(ASTNode* statement, ASTNode* statement_list)
-{
-    if (statement_list != NULL) {
-        statement_list->next = statement;
-        return statement_list;
-    } else {
-        return statement;
-    }
+// to form a bigger statement list node (add the statement to the statement
+// list). Return a pointer to the bigger list that resulted from this linking
+ASTNode *CreateStatementListNode(ASTNode *statement, ASTNode *statement_list) {
+  if (statement_list != NULL) {
+    statement_list->next = statement;
+    return statement_list;
+  } else {
+    return statement;
+  }
 }
 
-ASTNode* make_op(ASTOp operator, ASTNode* left_operand, ASTNode* right_operand)
-{
-    ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
-    node->op = operator;
-    node->left = left_operand;
-    node->right = right_operand;
-    return node;
+ASTNode *make_op(ASTOp operator, ASTNode *left_operand,
+                 ASTNode *right_operand) {
+  ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
+  node->op = operator;
+  node->left = left_operand;
+  node->right = right_operand;
+  return node;
 }
 
-void AddDeclaration(char* name)
-{
+void AddDeclaration(char *name) {}
 
-
-}
-
-// Commented out in this assignment 
+// Commented out in this assignment
 /*void GenerateILOC(ASTNode* node);
 {
 
 }*/
-
--- your_code.h
+++ your_code.h
@@ -1,38 +1,50 @@
 #ifndef YOUR_CODE_H
 #define YOUR_CODE_H
 
-typedef enum{ASTNODE_ARITH_OP, ASTNODE_LOGIC_OP, ASTNODE_COMPARE, ASTNODE_ASSIGN, ASTNODE_IDENT, ASTNODE_NUM, ASTNODE_IF, ASTNODE_WHILE} ASTNodeType;
+typedef enum {
+  ASTNODE_ARITH_OP,
+  ASTNODE_LOGIC_OP,
+  ASTNODE_COMPARE,
+  ASTNODE_ASSIGN,
+  ASTNODE_IDENT,
+  ASTNODE_NUM,
+  ASTNODE_IF,
+  ASTNODE_WHILE
+} ASTNodeType;
 
 // Define all operation types (add, mult, etc) here.
-typedef enum{ OP_ADD, OP_SUB, OP_MULT, OP_DIV } ASTOp; 
+typedef enum { OP_ADD, OP_SUB, OP_MULT, OP_DIV } ASTOp;
 
 typedef struct ASTNode ASTNode;
-struct ASTNode{
-	ASTNodeType type; 
-    ASTOp op; // The actual operation (add, mult, etc)
-	int num;  //Need to store the actual value for number nodes
-	char *name; //Need to store the actual variable name for ident nodes
-	ASTNode* left; // Left child
-	ASTNode* right; // Right child
-	
-	// The following pointer is just a suggestion; you don't have to use it if you have a better design.
-	ASTNode* next; // a pointer used to link statement nodes together in a statement list
+struct ASTNode {
+  ASTNodeType type;
+  ASTOp op;       // The actual operation (add, mult, etc)
+  int num;        // Need to store the actual value for number nodes
+  char *name;     // Need to store the actual variable name for ident nodes
+  ASTNode *left;  // Left child
+  ASTNode *right; // Right child
 
-// Depending on your implementation, you may need to add other fields to this struct 
+  // The following pointer is just a suggestion; you don't have to use it if you
+  // have a better design.
+  ASTNode *next; // a pointer used to link statement nodes together in a
+                 // statement list
 
+  // Depending on your implementation, you may need to add other fields to this
+  // struct
 };
 
-// Add functions to create the different kinds of ASTNodes 
+// Add functions to create the different kinds of ASTNodes
 // These functions are called by the code embedded in the grammar.
-// Here are some samples:  
-ASTNode* CreateNumNode(int num);
-ASTNode* CreateIdentNode(char* name);
-ASTNode* CreateStatementListNode(ASTNode* st, ASTNode* stList);
-ASTNode* make_op(ASTOp operator, ASTNode* left_operand, ASTNode* right_operand);
+// Here are some samples:
+ASTNode *CreateNumNode(int num);
+ASTNode *CreateIdentNode(char *name);
+ASTNode *CreateStatementListNode(ASTNode *st, ASTNode *stList);
+ASTNode *make_op(ASTOp operator, ASTNode *left_operand, ASTNode *right_operand);
 
 // Need a function to add a declaration to your symbol table
-void AddDeclaration(char* name);
+void AddDeclaration(char *name);
 
-// This is the function that generates ILOC code after the construction of the AST
-//void GenerateILOC(ASTNode* node);
+// This is the function that generates ILOC code after the construction of the
+// AST
+// void GenerateILOC(ASTNode* node);
 #endif
