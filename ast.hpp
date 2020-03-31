5fd90d002d02a208e9885cbb21504454c6a079b4
Michael Dorst <m@mdorst.net>
2020-03-31 15:17:36 -0700

    Clean up ast.hpp

--- ast.hpp
+++ ast.hpp
@@ -1,7 +1,7 @@
 #ifndef YOUR_CODE_H
 #define YOUR_CODE_H
 
-typedef enum {
+enum ASTNodeType {
   ASTNODE_ARITH_OP,
   ASTNODE_LOGIC_OP,
   ASTNODE_COMPARE,
@@ -10,43 +10,27 @@ typedef enum {
   ASTNODE_NUM,
   ASTNODE_IF,
   ASTNODE_WHILE
-} ASTNodeType;
+};
 
-// Define all operation types (add, mult, etc) here.
-typedef enum { OP_ADD, OP_SUB, OP_MULT, OP_DIV } ASTOp;
+enum ASTOp { OP_ADD, OP_SUB, OP_MULT, OP_DIV };
 
-typedef struct ASTNode ASTNode;
 struct ASTNode {
   ASTNodeType type;
-  ASTOp op;       // The actual operation (add, mult, etc)
-  int num;        // Need to store the actual value for number nodes
-  char *name;     // Need to store the actual variable name for ident nodes
-  ASTNode *left;  // Left child
-  ASTNode *right; // Right child
-
-  // The following pointer is just a suggestion; you don't have to use it if you
-  // have a better design.
-  ASTNode *next; // a pointer used to link statement nodes together in a
-                 // statement list
-
-  // Depending on your implementation, you may need to add other fields to this
-  // struct
+  ASTOp op;
+  int num;
+  char *name;
+  ASTNode *left;
+  ASTNode *right;
+  ASTNode *next;
 };
 
 extern ASTNode *g_ast_root;
 
-// Add functions to create the different kinds of ASTNodes
-// These functions are called by the code embedded in the grammar.
-// Here are some samples:
 ASTNode *CreateNumNode(int num);
 ASTNode *CreateIdentNode(char *name);
 ASTNode *CreateStatementListNode(ASTNode *st, ASTNode *stList);
 ASTNode *make_op(ASTOp op, ASTNode *left_operand, ASTNode *right_operand);
 
-// Need a function to add a declaration to your symbol table
 void AddDeclaration(char *name);
 
-// This is the function that generates ILOC code after the construction of the
-// AST
-// void GenerateILOC(ASTNode* node);
 #endif
