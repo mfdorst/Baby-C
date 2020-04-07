fb84357ee67aeeb26748cc8bd650262436f8ff69
Michael Dorst <m@mdorst.net>
2020-04-07 16:27:12 -0700

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+alias mc="make clean && make"
+
+m() {
+  # If no argument was passed, default to 1
+  [ $# -eq 0 ] && set $1 1
+  make && echo && ./bcc tests/test$1.bc
+}
+
+rem() {
+  # If no argument was passed, default to 1
+  [ $# -eq 0 ] && set $1 1
+  make clean && make && echo && ./bcc tests/test$1.bc
+}
+
+t() {
+  # If no argument was passed, default to 1
+  [ $# -eq 0 ] && set $1 1
+  ./bcc tests/test$1.bc > bcc.out
+  diff bcc.out tests/test$1_result.txt
+}
