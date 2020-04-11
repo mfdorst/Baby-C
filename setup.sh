71702ce97460d5370fd7c84fffbb7c3dbd14a321
Michael Dorst <m@mdorst.net>
2020-04-10 17:46:45 -0700

    Expand the test command to test code generation

--- setup.sh
+++ setup.sh
@@ -17,13 +17,16 @@ rem() {
 t() {
   # If no argument was passed, default to 1
   [ $# -eq 0 ] && set $1 1
+  echo "[Test $1]"
   ./bcc tests/test$1.bc >| bcc.out
+  echo "[Parser]"
   diff bcc.out tests/test$1_result.txt
+  echo "[Code gen]"
+  diff tests/test$1.iloc tests/test$1_ref.iloc
 }
 
 ta() {
   for i in 1 2 3 4 5 6 7 8; do
-    echo Test $i:
     if ! t $i; then
       break
     fi
